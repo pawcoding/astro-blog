f47bea7aa9156a0b0cc6e83535ce6737386b263f
pawcode Development <development@pawcode.de>
2023-10-27 19:12:51 +0200

    feat(integrations): add matomo and sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,27 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
-
 import mdx from "@astrojs/mdx";
+import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import matomo from "astro-matomo";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), mdx()]
+  site: "https://blog.pawcode.de",
+  integrations: [
+    tailwind(),
+    preact(),
+    mdx(),
+    matomo({
+      enabled: import.meta.env.PROD,
+      host: "https://analytics.apps.pawcode.de/",
+      siteId: 9,
+      disableCookies: true,
+    }),
+    sitemap({
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmod: new Date()
+    }),
+  ],
 });
