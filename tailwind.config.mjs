5dbbea2d883fac29d1965ba43c680c87da62d3df
pawcode Development <development@pawcode.de>
2023-10-27 14:02:11 +0200

    feat(layout): add header and footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      blur: {
+        xs: '2px'
+      },
+      height: {
+        'screen-95': '95svh'
+      },
+      minHeight: {
+        screen: '100svh'
+      },
+      margin: {
+        '3/4': '0.1875rem'
+      }
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
