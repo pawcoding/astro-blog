cc9e43f74a7398b807cdbb2f903c757dac695894
pawcode Development <development@pawcode.de>
2023-12-05 09:53:25 +0100

    ci(vercel): only deploy on release commits

---
+++ build_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if commit is a release commit
+git show --oneline -s HEAD | grep 'chore(release):' 2>/dev/null
+
+if [ $? -eq 0 ]; then
+    # Build if release commit
+    echo "✅ - Build can proceed"
+    exit 1
+fi
+
+# Cancel build if not release commit
+echo "🛑 - Master build, cancelled"
+exit 0
